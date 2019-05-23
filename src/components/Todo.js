18b9eedfeefa255d7b5e0b78aa577b08c77b1beb
Jack Gamboa <jgamboa929@gmail.com>
2019-05-23 09:45:51 -0500

    Todo.js - Add multiple states using useState and display.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,22 +1,32 @@
 import React, { useState } from "react";
 
 const todo = props => {
-  const inputState = useState("");
+  const [todoName, setTodoName] = useState("");
+  const [todoList, setTodoList] = useState([]);
 
   const inputChangeHandler = event => {
-    inputState[1](event.target.value);
+    setTodoName(event.target.value);
   };
+
+  const todoAddHandler = () => {
+    setTodoList(todoList.concat(todoName));
+  };
+
+  const displayTodo = () => todoList.map(todo => <li key={todo}>{todo}</li>);
 
   return (
     <React.Fragment>
       <input
+        className="i"
         type="text"
         placeholder="Todo"
         onChange={inputChangeHandler}
-        value={inputState[0]}
+        value={todoName}
       />
-      <button type="button">Add</button>
-      <ul />
+      <button type="button" onClick={todoAddHandler}>
+        Add
+      </button>
+      <ul>{displayTodo()}</ul>
     </React.Fragment>
   );
 };
